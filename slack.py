2766d5df541bec90ba09d527e7ddd6290c3cbb6c
rosscrooke <ross.crooke@bts.com>
2016-07-20 20:20:16 +0100

    Update slack.py

--- slack.py
+++ slack.py
@@ -27,54 +27,11 @@ def index():
         "icon_emoji": ":robot_face:",
     })
 
-    resp2 = slack.post("chat.postMessage", data={
-    "text": "New comic book alert!",
-    "attachments": [
-        {
-            "title": "The Further Adventures of Slackbot",
-            "fields": [
-                {
-                    "title": "Volume",
-                    "value": "1",
-                    "short": true
-                },
-                {
-                    "title": "Issue",
-                    "value": "3",
-            "short": true
-                }
-            ],
-            "author_name": "Stanford S. Strickland",
-            "author_icon": "https://api.slack.com/img/api/homepage_custom_integrations-2x.png",
-            "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"
-        },
-        {
-            "title": "Synopsis",
-            "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
-        },
-        {
-            "fallback": "Would you recommend it to customers?",
-            "title": "Would you recommend it to customers?",
-            "callback_id": "comic_1234_xyz",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "recommend",
-                    "text": "Recommend",
-                    "type": "button",
-                    "value": "recommend"
-                },
-                {
-                    "name": "no",
-                    "text": "No",
-                    "type": "button",
-                    "value": "bad"
-                }
-            ]
-        }
-    ]
-})
+    resp = slack.post("chat.postMessage", data={
+        "channel": "#general",
+        "text": "testing",
+        "icon_emoji": ":robot_face:",
+    })
     
     assert resp2.ok, resp2.text
     return resp2.text
